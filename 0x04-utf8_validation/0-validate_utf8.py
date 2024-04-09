5bd9da57852fa279b164c7ebb3b06e3db2de1dfc
Scott-cityboy <scottimbiaka@gmail.com>
2024-04-09 16:49:14 +0300

    0x04-utf8_validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,34 +1,13 @@
 #!/usr/bin/python3
+"""
+UTF8 validation module
+"""
+
 def validUTF8(data):
-    
-    remaining_bytes = 0
-    
-    
-    for byte in data:
-        
-        if remaining_bytes > 0:
-            if byte >> 6 != 0b10:
-                return False
-            remaining_bytes -= 1
-        else:
-            
-            mask = 0b10000000
-            while byte & mask:
-                remaining_bytes += 1
-                mask >>= 1
-            
-            
-            if remaining_bytes == 0:
-                if byte >> 7 == 0:
-                    continue
-                else:
-                    return False
-            elif remaining_bytes > 4 or remaining_bytes == 1:
-                return False
-            
-        
-        remaining_bytes -= 1
-    
-    
-    return remaining_bytes == 0
+    """
+    Validates if data consists of
+    valid utf-8 encoded characters
+    """
+    pass
+
 
